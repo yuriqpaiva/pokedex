e91b6b4488c9004cbeace836826bec2425814f34
yuriqpaiva <yuriqpaiva@gmail.com>
2021-08-26 09:17:00 -0300

    Update Content.tsx

--- src/components/Content.tsx
+++ src/components/Content.tsx
@@ -38,22 +38,25 @@ export default function Content(props: ContentProps) {
             return (
                 <li key={numPokemon}
                     className={`
-                    sm:m-3 my-4 xl:m-8 bg-gray-100 sm:p-4 rounded-lg border-2
+                    sm:m-2 sm:my-2 my-4 xl:m-8 bg-gray-100 sm:p-4 rounded-lg border-2
                     border-gray-300 shadow-sm hover:shadow-xl 
                     hover:scale-75 cursor-pointer focus:shadow-xl   
                     focus:outline-none hover:border-red-400 
                     focus:border-red-400
-                    `}  
+                    `}
                     tabIndex={0}
-                    >
+                >
                     <span className={`
                     block mb-1 text-red-900 font-semibold
                     `}>{`#${numPokemon}`}</span>
                     <img
                         src={pokemon.url}
                         alt={pokemon.name}
-                        className={`sm:w-36 sm:h-36 w-24 h-24 mb-2`}
-                    />                    
+                        className={`
+                            sm:w-36 sm:h-36  
+                            w-20 h-20 mb-2
+                        `}
+                    />
                     <span className={`sm:text-xl text-gray-700`}>{`${formatedPokemon}`}</span>
                 </li>
             )
@@ -69,12 +72,12 @@ export default function Content(props: ContentProps) {
         mt-5 w-full sm:px-20 px-10 py-8 rounded-lg shadow-sm 
         bg-white `}>
             <h2>{props.title}</h2>
-                <ul className={`
+            <ul className={`
                 flex flex-wrap justify-between 
                 text-center sm:text-base text-sm 
                 `}>
-                    {listaPokemons(pokemons)}
-                </ul>
+                {listaPokemons(pokemons)}
+            </ul>
         </div>
     )
 }
