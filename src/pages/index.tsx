f5e5f4fe0a9f829fddfa1b2a5be1387de58df7c8
yuriqpaiva <yuriqpaiva@gmail.com>
2021-08-29 17:33:26 -0300

    show more button correction

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -7,22 +7,24 @@ import { usePokemonData } from '../hook/usePokemonData'
 
 export default function Home() {
 
-  const { search, setShowAll, showAll } = usePokemonData()
+  const { search, setShowAll, showAll, loading } = usePokemonData()
 
   return (
-      <Layout skip>
-        <Input
-          title='Pesquisar:'
-          placeholder='Insira o nome do Pokémon'
-          onChange={search}
-        />
-        <Line className='mb-5'/>
-        <PokeList />
-        {showAll ? false : (
-          <Button label='Mostrar Mais' 
-          className='mt-5' onClick={setShowAll} />
+    <Layout skip>
+      <Input
+        title='Pesquisar:'
+        placeholder='Insira o nome do Pokémon'
+        onChange={search}
+      />
+      <Line className='mb-5' />
+      <PokeList />
+      {showAll ? false : (
+        loading ? false : (
+          <Button label='Mostrar Mais'
+            className='mt-5' onClick={setShowAll} />
+        )
         )}
-        <Line className='mt-5'/>
-      </Layout>
+      <Line className='mt-5' />
+    </Layout>
   )
 }
